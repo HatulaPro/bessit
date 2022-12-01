a8cfc95d2e057b57e1870b7c55f6ce1c1b3fa241
Shalev Raz Israel <shalev280804@gmail.com>
2022-12-01 13:23:11 +0200

    simplifying community

--- src/pages/b/[community_name].tsx
+++ src/pages/b/[community_name].tsx
@@ -1,3 +1,4 @@
+import { Community } from "@prisma/client";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
@@ -53,11 +54,23 @@ const CommunityPageContent: React.FC<{ name: string }> = ({ name }) => {
   }
   return (
     <div className="w-full">
+      <CommunityHeader community={communityQuery.data} />
+      <div className="container mx-auto flex max-w-5xl items-start justify-center gap-16 px-0 md:px-2">
+        <CommunityPosts community={communityQuery.data} />
+        <AboutCommunity community={communityQuery.data} />
+      </div>
+    </div>
+  );
+};
+
+const CommunityHeader: React.FC<{ community: Community }> = ({ community }) => {
+  return (
+    <>
       <div className="bg-rotate h-40 w-full bg-gradient-radial from-stone-700 bg-[length:12px_12px] md:h-64">
-        {communityQuery.data.image && (
+        {community.image && (
           <Image
-            src={communityQuery.data.image}
-            alt={`Community image of ${communityQuery.data.name}`}
+            src={community.image}
+            alt={`Community image of ${community.name}`}
             fill
           />
         )}
@@ -65,38 +78,46 @@ const CommunityPageContent: React.FC<{ name: string }> = ({ name }) => {
       <div className="mb-2 flex w-full flex-col items-center justify-center bg-black">
         <div className="flex w-full max-w-xl -translate-y-1/3 items-center gap-6 md:max-w-3xl">
           <div className="flex h-20 w-20 flex-shrink-0 flex-col items-center justify-center rounded-full border-2 border-zinc-400 bg-indigo-800 text-5xl md:h-24 md:w-24 md:text-7xl">
-            {communityQuery.data.logo ? (
+            {community.logo ? (
               <Image
-                src={communityQuery.data.logo}
-                alt={`Community logo of ${communityQuery.data.name}`}
+                src={community.logo}
+                alt={`Community logo of ${community.name}`}
                 fill
               />
             ) : (
-              communityQuery.data.name[0]?.toUpperCase()
+              community.name[0]?.toUpperCase()
             )}
           </div>
-          <h1 className="text-3xl">{communityQuery.data.name}</h1>
+          <h1 className="text-3xl">{community.name}</h1>
         </div>
       </div>
-      <div className="container mx-auto flex max-w-5xl items-start justify-center gap-16 px-0 md:px-2">
-        <div className="flex-[2] border-[1px] border-x-0 border-zinc-400 bg-zinc-800 p-4 md:rounded-md md:border-x-[1px]">
-          <h2 className="text-center text-xl text-zinc-400">Posts</h2>
-          {new Array(25).fill(0).map((_, i) => (
-            <p key={i}>A lot of data</p>
-          ))}
-        </div>
-        <div className="hidden flex-1 rounded-md border-[1px] border-zinc-400 bg-zinc-800 p-4 md:block">
-          <h2 className="text-center text-xl text-zinc-400">About Community</h2>
-          <p>{communityQuery.data.desc}</p>
-          <hr className="m-2" />
-          <Link
-            href="/"
-            className="mx-auto block w-2/3 rounded-lg bg-zinc-300 p-1 text-center font-bold text-black hover:bg-zinc-400 active:bg-zinc-500"
-          >
-            Create Post
-          </Link>
-        </div>
-      </div>
+    </>
+  );
+};
+
+const AboutCommunity: React.FC<{ community: Community }> = ({ community }) => {
+  return (
+    <div className="hidden flex-1 rounded-md border-[1px] border-zinc-400 bg-zinc-800 p-4 md:block">
+      <h2 className="text-center text-xl text-zinc-400">About Community</h2>
+      <p>{community.desc}</p>
+      <hr className="m-2" />
+      <Link
+        href="/"
+        className="mx-auto block w-2/3 rounded-lg bg-zinc-300 p-1 text-center font-bold text-black hover:bg-zinc-400 active:bg-zinc-500"
+      >
+        Create Post
+      </Link>
+    </div>
+  );
+};
+
+const CommunityPosts: React.FC<{ community: Community }> = ({ community }) => {
+  return (
+    <div className="flex-[2] border-[1px] border-x-0 border-zinc-400 bg-zinc-800 p-4 md:rounded-md md:border-x-[1px]">
+      <h2 className="text-center text-xl text-zinc-400">Posts</h2>
+      {new Array(25).fill(0).map((_, i) => (
+        <p key={i}>A lot of data for {community.name}</p>
+      ))}
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { PostEditor } from "../components/PostEditor";
 import { TopBar } from "../components/TopBar";
 
 const Home: NextPage = () => {
@@ -24,27 +25,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-const PostEditor: React.FC = () => {
-  return (
-    <div className="flex w-full max-w-3xl flex-col items-center gap-2 rounded border-2 border-zinc-800 p-8">
-      <div className="flex w-full">
-        <h2 className="my-2 w-full text-3xl text-white">Create a post</h2>
-      </div>
-      <hr className="my-2" />
-      <input
-        className="w-full rounded border-2 border-zinc-500 bg-transparent p-1 text-zinc-200 outline-none focus:border-zinc-300"
-        type="text"
-        placeholder="Title"
-      />
-      <textarea
-        className="min-h-[2.4rem] w-full overflow-y-scroll rounded border-2 border-zinc-500 bg-transparent p-1 text-zinc-200 outline-none focus:border-zinc-300"
-        placeholder="Your awesome post"
-      ></textarea>
-
-      <button className="mt-4 w-24 rounded bg-indigo-700 p-2 text-white">
-        Create
-      </button>
-    </div>
-  );
-};
