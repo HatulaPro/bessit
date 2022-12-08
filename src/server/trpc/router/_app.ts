badcc1fc7d4c94f808fe4180a06ed6d20ce8af98
Shalev Raz Israel <shalev280804@gmail.com>
2022-12-08 09:28:53 +0200

    Removing boilerplate t3 code

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,11 +1,9 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
 import { communitiesRouter } from "./communities";
 import { postsRouter } from "./posts";
 
 export const appRouter = router({
-  example: exampleRouter,
   auth: authRouter,
   community: communitiesRouter,
   post: postsRouter,
