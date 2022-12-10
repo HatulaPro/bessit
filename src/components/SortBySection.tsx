6d20b4dd2cd261cbf14cc61d5bf9483484aa8154
Shalev Raz Israel <shalev280804@gmail.com>
2022-12-10 12:43:25 +0200

    sorting options now look better

--- src/components/SortBySection.tsx
+++ src/components/SortBySection.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import { cx } from "../utils/general";
 import { AiFillCaretDown } from "react-icons/ai";
 import type { RouterInputs } from "../utils/trpc";
+import { BsClock, BsExclamationTriangle, BsStars } from "react-icons/bs";
+import type { IconType } from "react-icons/lib";
 
 export type SortingOptions = RouterInputs["post"]["getPosts"]["sort"];
 export type PostsFromLastOptions =
@@ -15,6 +17,12 @@ const TIME_FILTERS: Record<PostsFromLastOptions, string> = {
   "all time": "All Time",
 };
 
+const SORT_OPTIONS: Record<SortingOptions, IconType> = {
+  new: BsClock,
+  moot: BsExclamationTriangle,
+  hot: BsStars,
+};
+
 export const SortBySection: React.FC<{
   setSortBy: (val: SortingOptions) => void;
   sortBy: SortingOptions;
@@ -22,93 +30,88 @@ export const SortBySection: React.FC<{
   timeFilter: PostsFromLastOptions;
   isLoading: boolean;
 }> = ({ timeFilter, setTimeFilter, setSortBy, sortBy, isLoading }) => {
-  const activeSortByRef = useRef<HTMLButtonElement>(null);
   const [isFocused, setFocused] = useState<boolean>(false);
 
-  useEffect(() => {
-    if (activeSortByRef.current) {
-      const activeButton = activeSortByRef.current;
-      const parent = activeButton.parentElement as HTMLDivElement;
-      const buttonRect = activeButton.getBoundingClientRect();
-      const parentRect = (
-        parent.parentElement as HTMLDivElement
-      ).getBoundingClientRect();
-      const desiredCenter = parentRect.width / 2 + parentRect.left;
-      const buttonCenter = buttonRect.width / 2 + buttonRect.left;
-      parent.scrollBy({
-        behavior: "smooth",
-        left: buttonCenter - desiredCenter,
-      });
+  function setCorrectSortingOption(val: SortingOptions) {
+    if (val === "moot") {
+      setTimeFilter("day");
+    } else if (val === "new") {
+      setTimeFilter("all time");
+    } else if (val === "hot") {
+      setTimeFilter("day");
     }
-  }, [sortBy, activeSortByRef, isLoading]);
+
+    setSortBy(val);
+  }
 
   return (
-    <div className="w-full max-w-3xl bg-zinc-900 pb-2 text-white">
-      <div
-        className="relative ml-auto mr-2 mt-4 mb-3 w-32 text-base md:text-lg"
-        onBlur={(e) => {
-          if (!e.relatedTarget || !e.currentTarget.contains(e.relatedTarget)) {
-            setFocused(false);
-          }
-        }}
-      >
+    <div className="justify-left mt-4 flex w-full max-w-3xl flex-1 items-center gap-2 bg-zinc-800 px-0.5 py-3 text-white md:rounded-md md:px-2">
+      {Object.entries(SORT_OPTIONS).map(([sortFilter, Icon]) => (
         <button
-          onClick={() => setFocused(!isFocused)}
           className={cx(
-            "flex w-full items-center justify-evenly border-[1px] border-zinc-500 py-1.5 hover:enabled:bg-zinc-800",
-            isFocused ? "rounded-t" : "rounded",
-            isLoading && "contrast-50"
+            "my-0.5 flex items-center gap-1 rounded-full py-0.5 px-2 text-base font-bold transition-all disabled:opacity-50 disabled:contrast-50 md:px-3 md:text-lg",
+            sortFilter === sortBy
+              ? "bg-zinc-700 text-white"
+              : "bg-transparent text-zinc-500 hover:bg-zinc-700"
           )}
+          key={sortFilter}
+          onClick={() => {
+            setCorrectSortingOption(sortFilter as keyof typeof SORT_OPTIONS);
+          }}
           disabled={isLoading}
         >
-          {TIME_FILTERS[timeFilter]}
-          <AiFillCaretDown className="text-sm md:text-base" />
+          <Icon />
+          {sortFilter}
         </button>
+      ))}
+      {sortBy === "hot" && (
         <div
-          className={cx(
-            "absolute top-full z-20 max-h-44 w-full origin-top overflow-y-auto rounded-b border-[1px] border-t-0 border-zinc-500 bg-zinc-900 transition-transform",
-            isFocused && !isLoading ? "scale-y-100" : "scale-y-0"
-          )}
+          className="relative ml-auto mr-2 w-24 text-sm md:w-28 md:text-base"
+          onBlur={(e) => {
+            if (
+              !e.relatedTarget ||
+              !e.currentTarget.contains(e.relatedTarget)
+            ) {
+              setFocused(false);
+            }
+          }}
         >
-          {Object.entries(TIME_FILTERS).map(
-            ([apiName, displayName]) =>
-              apiName !== timeFilter && (
-                <button
-                  key={apiName}
-                  className="w-full border-t-[1px] border-t-zinc-700 py-1.5 hover:bg-zinc-800"
-                  onClick={() => {
-                    setTimeFilter(apiName as keyof typeof TIME_FILTERS);
-                    setFocused(false);
-                  }}
-                >
-                  {displayName}
-                </button>
-              )
-          )}
-        </div>
-      </div>
-      <div className="hidden-scroller justify-left flex w-full flex-1 gap-2 overflow-scroll">
-        <div className="w-[60%] shrink-0 basis-[60%]"></div>
-        {(["new", "hot", "moot"] as const).map((value) => (
           <button
+            onClick={() => setFocused(!isFocused)}
             className={cx(
-              "text-md rounded-full py-1 px-5 transition-all disabled:opacity-50 disabled:contrast-50 md:px-6 md:text-lg",
-              value === sortBy
-                ? "bg-zinc-100 text-black"
-                : "bg-zinc-600 text-white"
+              "flex w-full items-center justify-evenly border-[1px] border-zinc-500 py-1 hover:enabled:bg-zinc-800",
+              isFocused ? "rounded-t" : "rounded",
+              isLoading && "contrast-50"
             )}
-            key={value}
-            onClick={() => {
-              setSortBy(value);
-            }}
-            ref={value === sortBy ? activeSortByRef : undefined}
             disabled={isLoading}
           >
-            {value}
+            {TIME_FILTERS[timeFilter]}
+            <AiFillCaretDown className="text-sm md:text-base" />
           </button>
-        ))}
-        <div className="w-[60%] shrink-0 basis-[60%]"></div>
-      </div>
+          <div
+            className={cx(
+              "absolute top-full z-20 max-h-44 w-full origin-top overflow-y-auto rounded-b border-[1px] border-t-0 border-zinc-500 bg-zinc-900 transition-transform",
+              isFocused && !isLoading ? "scale-y-100" : "scale-y-0"
+            )}
+          >
+            {Object.entries(TIME_FILTERS).map(
+              ([apiName, displayName]) =>
+                apiName !== timeFilter && (
+                  <button
+                    key={apiName}
+                    className="w-full border-t-[1px] border-t-zinc-700 py-1 hover:bg-zinc-800"
+                    onClick={() => {
+                      setTimeFilter(apiName as keyof typeof TIME_FILTERS);
+                      setFocused(false);
+                    }}
+                  >
+                    {displayName}
+                  </button>
+                )
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
