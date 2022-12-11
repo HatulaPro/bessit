e0fd0c3957181a606dfa0fce28c22d3e2eeac4ea
Shalev Raz Israel <shalev280804@gmail.com>
2022-12-11 09:30:28 +0200

    Created the LoggedOnlyButton component

---
+++ src/components/LoggedOnlyButton.tsx
@@ -0,0 +1,74 @@
+import { signIn, useSession } from "next-auth/react";
+import React, { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import { Dialog } from "./Dialog";
+
+export function LoggedOnlyButton<T extends React.FC>({
+  Child,
+  onClick,
+  icon,
+  title,
+  content,
+}: {
+  Child: T; // The button to be wrapped
+  onClick: React.MouseEventHandler; // An event handler for when the user is logged in
+  icon: React.ReactNode; // Recommended: react-icons icon, with only a *text-color* style.
+  title: React.ReactNode; // The title of the dialog when opened, wrap in a fragment
+  content: string; // The content of the dialog when opened
+}) {
+  const [isOpen, setOpen] = useState<boolean>(false);
+  const { status: authStatus } = useSession();
+
+  const isLoggedIn = authStatus === "authenticated";
+
+  return (
+    <>
+      {!isLoggedIn && (
+        <Dialog isOpen={isOpen} close={() => setOpen(false)}>
+          <div className="relative m-auto w-full max-w-md rounded bg-zinc-800 p-8 px-6 text-center text-white">
+            <button
+              className="absolute top-2 left-2 mr-auto rounded-full text-2xl text-white hover:bg-white hover:bg-opacity-20"
+              onClick={() => setOpen(false)}
+            >
+              <IoMdClose />
+            </button>
+            <div className="mx-auto mb-3 w-min text-center text-4xl">
+              {icon}
+            </div>
+            <h2 className="py-2 text-2xl">{title}</h2>
+            <hr className="my-1.5 opacity-25" />
+            <p className="p-4 text-start text-zinc-400">{content}</p>
+
+            <button
+              className="w-full rounded bg-indigo-700 py-2 px-6 text-lg text-white"
+              onClick={() => signIn()}
+            >
+              Log In
+            </button>
+          </div>
+        </Dialog>
+      )}
+      {Child({ onClick: isLoggedIn ? onClick : () => setOpen(true) })}
+    </>
+  );
+}
+
+// Usage example:
+/* <LoggedOnlyButton
+  Child={(props) => {
+    return <button {...props}>hello</button>;
+  }}
+  onClick={(e) => console.log(e)}
+  icon={<BsSuitHeartFill className="text-red-500" />}
+  title={
+    <>
+      Like{" "}
+      <Link href="/" className="font-bold hover:underline">
+        Hatula Pro
+      </Link>
+      &apos;s great post
+    </>
+  }
+  content="Join Bessit to let the world know of your appreciation of awesome content"
+/>; 
+*/
