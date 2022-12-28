2980d8445374d4f70f75f50f242cef77c8550676
Shalev Raz Israel <shalev280804@gmail.com>
2022-12-28 14:01:58 +0200

    fix

--- src/components/CommunityLogo.tsx
+++ src/components/CommunityLogo.tsx
@@ -10,12 +10,12 @@ export const CommunityLogo: React.FC<{
   return (
     <div
       className={cx(
-        "relative flex-shrink-0 items-center justify-center rounded-full border-2 border-zinc-400 bg-indigo-800 text-center",
+        "relative flex items-center justify-center overflow-hidden rounded-full bg-indigo-800",
         size === "large"
-          ? "flex h-20 w-20 text-5xl md:h-24 md:w-24 md:text-7xl"
+          ? "h-20 w-20 text-5xl md:h-24 md:w-24 md:text-7xl"
           : size === "small"
-          ? "flex h-5 w-5 text-xs md:h-6 md:w-6 md:text-sm"
-          : "md: flex h-7 w-7 text-base md:h-8 md:w-8 md:text-lg",
+          ? "h-5 w-5 text-xs md:h-7 md:w-7 md:text-sm"
+          : "h-7 w-7 text-base md:h-8 md:w-8 md:text-lg",
         placeholder && "animate-pulse"
       )}
     >
@@ -24,7 +24,7 @@ export const CommunityLogo: React.FC<{
           loader={({ src }) => src}
           src={logo}
           alt={`Community logo of ${name}`}
-          className="rounded-full object-cover"
+          className="object-cover"
           fill
           priority
         />
