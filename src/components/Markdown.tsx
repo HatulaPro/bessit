324bcf9a72aceaa93bbf95d554439ae473b2b858
Shalev Raz Israel <shalev280804@gmail.com>
2022-12-29 23:47:13 +0200

    You can now tag users

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,19 +1,69 @@
 import ReactMarkdown from "react-markdown";
-import { cx } from "../utils/general";
+import { cx, slugify } from "../utils/general";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 import rehypeSanitize from "rehype-sanitize";
 import { useMemo } from "react";
+import { trpc } from "../utils/trpc";
 
 const communityNameRegex = /(^|\s)(\/b\/[a-z0-9_]{2,24})($|\s)/gm;
+const userIdRegex =
+  /(^|\s)(\/u\/([a-zA-Z0-9\.@*\$\/\\\#]{2,32}|\(([^ ][a-zA-Z0-9\.@*\$\/\\\# ]{1,32})\)))($|\s)/gm;
 export const Markdown: React.FC<{ source: string; simplify?: boolean }> = ({
   source,
   simplify,
 }) => {
-  const memoizedSource = useMemo(
-    () => source.replaceAll(communityNameRegex, '$1<a href="$2">$2</a>$3'),
+  const usersMatched = useMemo(
+    () =>
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      [...source.matchAll(userIdRegex)].map((match) => match[4] ?? match[3]!),
     [source]
   );
+  console.log(usersMatched);
+  const userQueries = trpc.useQueries((t) => {
+    return usersMatched.map((u) =>
+      t.search.search(
+        { q: `/u/${u}` },
+        {
+          refetchOnReconnect: false,
+          refetchOnWindowFocus: false,
+          cacheTime: Infinity,
+          staleTime: Infinity,
+        }
+      )
+    );
+  });
+
+  console.log(userQueries);
+  const usersMap = useMemo(() => {
+    const res = new Map();
+    for (const userQuery of userQueries) {
+      if (userQuery.data?.users?.length) {
+        const user = userQuery.data.users[0];
+        if (user?.name) {
+          res.set(user.name, user);
+        }
+      }
+    }
+    return res;
+  }, [userQueries]);
+
+  const memoizedSource = useMemo<string>(() => {
+    const newSource = source
+      .replaceAll(communityNameRegex, '$1<a href="$2">$2</a>$3')
+      .replaceAll(userIdRegex, (a, b, _c, d, e, f) => {
+        const username = e ?? d;
+        if (usersMap.has(username)) {
+          return `${b}<a data-is-user-link="true" href="/u/${
+            usersMap.get(e ?? d)?.id
+          }/${slugify(username)}">/u/${username}</a>${f}`;
+        } else {
+          return a;
+        }
+      });
+    return newSource;
+  }, [source, usersMap]);
+
   return (
     <ReactMarkdown
       className={cx(
