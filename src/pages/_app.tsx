e52f9c9f67a71230bbbc8d6cb00af591d573e1ad
Shalev Raz Israel <shalev280804@gmail.com>
2022-12-31 22:57:25 +0200

    Reducing first load JS by a shitload across the board

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
 import { trpc } from "../utils/trpc";
-
 import "../styles/globals.css";
-import { TopBar } from "../components/TopBar";
+import dynamic from "next/dynamic";
+
+const TopBar = dynamic(
+  () => import("../components/TopBar").then((x) => x.TopBar),
+  {
+    loading: () => (
+      <div className="fixed top-0 z-50 flex h-12 w-full flex-row-reverse justify-between bg-zinc-700 p-2 text-white md:flex-row"></div>
+    ),
+  }
+);
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
