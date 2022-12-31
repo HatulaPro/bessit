b726ad541c26470a60c9c4fedd650d714f277fd1
Shalev Raz Israel <shalev280804@gmail.com>
2022-12-31 23:22:47 +0200

    More perf

--- src/components/PostsViewer.tsx
+++ src/components/PostsViewer.tsx
@@ -20,10 +20,6 @@ import { useSession } from "next-auth/react";
 import { CommunityLogo } from "./CommunityLogo";
 import { useEffect, useMemo, useRef, useState } from "react";
 import { Dialog } from "./Dialog";
-import { useForm, Controller } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Tabs } from "./Tabs";
 import { trpc } from "../utils/trpc";
 import { UserProfileLink } from "./UserProfileLink";
 import { useRouter } from "next/router";
@@ -31,6 +27,9 @@ import { NotBannedOnlyButton } from "./NotBannedOnlyButton";
 import dynamic from "next/dynamic";
 
 const Markdown = dynamic(() => import("./Markdown").then((x) => x.Markdown));
+const EditPostForm = dynamic(() =>
+  import("./EditPostForm").then((x) => x.EditPostForm)
+);
 
 export const PostsViewer: React.FC<{ communityPosts: CommunityPosts }> = ({
   communityPosts,
@@ -412,129 +411,5 @@ const EditPostButton: React.FC<{ post: CommunityPosts["posts"][number] }> = ({
         <EditPostForm post={post} close={() => setOpen(false)} />
       </Dialog>
     </>
-  );
-};
-
-const editPostSchema = z.object({
-  title: z
-    .string()
-    .min(2, { message: "Title must have at least 2 characters" })
-    .max(256, { message: "Title must have at most 256 characters" }),
-  content: z
-    .string()
-    .max(4096, { message: "Content must have at most 4096 characters" }),
-});
-type editPostForm = z.infer<typeof editPostSchema>;
-const EditPostForm: React.FC<{
-  post: CommunityPosts["posts"][number];
-  close: () => void;
-}> = ({ post, close }) => {
-  const { control, handleSubmit, formState } = useForm<editPostForm>({
-    mode: "onTouched",
-    resolver: zodResolver(editPostSchema),
-    defaultValues: {
-      content: post.content,
-      title: post.title,
-    },
-  });
-
-  const editPostMutation = trpc.post.editPost.useMutation();
-  const utils = trpc.useContext();
-
-  const onSubmit = (data: editPostForm) => {
-    editPostMutation.mutate({ ...data, postId: post.id });
-    utils.post.getPosts.invalidate();
-    utils.post.getPost.setData({ post_id: post.id }, () => ({
-      ...post,
-      ...data,
-    }));
-    close();
-  };
-
-  return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="my-auto flex w-full max-w-3xl flex-col items-center gap-1 rounded border-2 border-transparent bg-zinc-900 p-2 text-white md:border-zinc-800 md:p-4"
-    >
-      <div className="flex w-full items-center">
-        <h2 className="my-2 w-full text-lg text-white md:text-2xl">
-          Update Post
-        </h2>
-        <button
-          type="submit"
-          className="text-md w-16 rounded bg-indigo-700 p-2 text-white disabled:bg-indigo-500 disabled:text-gray-400 md:w-24 md:text-lg"
-          disabled={!formState.isValid || editPostMutation.isLoading}
-        >
-          Save
-        </button>
-      </div>
-      <Loading size="small" show={editPostMutation.isLoading} />
-      <hr className="my-2 w-full" />
-      <div className="visible flex max-h-[250vh] w-full origin-top scale-y-100 flex-col gap-2 transition-all">
-        <Controller
-          control={control}
-          name="title"
-          render={({ field, fieldState }) => (
-            <>
-              <div
-                className={cx(
-                  "overflow-hidden text-red-400 transition-all",
-                  fieldState.error?.message ? "h-8" : "h-0"
-                )}
-              >
-                {fieldState.error?.message}
-              </div>
-              <input
-                autoComplete="off"
-                {...field}
-                className={cx(
-                  "w-full rounded border-2 bg-transparent p-1 text-zinc-200 outline-none",
-                  fieldState.error
-                    ? "border-red-600"
-                    : "border-zinc-500 focus:border-zinc-300"
-                )}
-                type="text"
-                placeholder="Title"
-              />
-            </>
-          )}
-        />
-        <Controller
-          control={control}
-          name="content"
-          render={({ field, fieldState }) => (
-            <Tabs
-              defaultValue="editor"
-              data={{
-                editor: (
-                  <>
-                    <div
-                      className={cx(
-                        "overflow-hidden text-red-400 transition-all",
-                        fieldState.error?.message ? "h-8" : "h-0"
-                      )}
-                    >
-                      {fieldState.error?.message}
-                    </div>
-                    <textarea
-                      autoComplete="off"
-                      {...field}
-                      className={cx(
-                        "max-h-[50vh] min-h-[2.4rem] w-full overflow-y-scroll rounded border-2 bg-transparent p-1 text-zinc-200 outline-none",
-                        fieldState.error
-                          ? "border-red-600"
-                          : "border-zinc-500 focus:border-zinc-300"
-                      )}
-                      placeholder="Your awesome post"
-                    ></textarea>
-                  </>
-                ),
-                preview: <Markdown source={field.value} />,
-              }}
-            />
-          )}
-        />
-      </div>
-    </form>
   );
 };
--- src/pages/b/[community_name]/post/[...post_data].tsx
+++ src/pages/b/[community_name]/post/[...post_data].tsx
@@ -13,8 +13,6 @@ import { type RouterOutputs, trpc } from "../../../../utils/trpc";
 import superjson from "superjson";
 import { IoMdClose } from "react-icons/io";
 import type { CommunityPosts } from "../../../../hooks/useCommunityPosts";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm, Controller } from "react-hook-form";
 import { cx, slugify, timeAgo } from "../../../../utils/general";
 import { useSession } from "next-auth/react";
 import { useEffect, useMemo, useRef, useState } from "react";
@@ -39,6 +37,12 @@ import dynamic from "next/dynamic";
 
 const Markdown = dynamic(() =>
   import("../../../../components/Markdown").then((x) => x.Markdown)
+);
+
+const CreateCommentForm = dynamic(() =>
+  import("../../../../components/CreateCommentForm").then(
+    (x) => x.CreateCommentForm
+  )
 );
 
 const PostPage: NextPage = () => {
@@ -454,143 +458,6 @@ const PostComments: React.FC<{
           </div>
         );
       })}
-    </div>
-  );
-};
-
-const createCommentSchema = z.object({
-  content: z
-    .string()
-    .min(4, { message: "Comment must be at least 4 characters long" })
-    .max(4096, { message: "Comment must be at most 4096 characters long" }),
-});
-type createCommentForm = z.infer<typeof createCommentSchema>;
-
-const CreateCommentForm: React.FC<{
-  postId: string;
-  parentCommentId: string | null;
-  commentContent: string;
-  setCurrentParentCommentId: (x: string | null) => void;
-  editOrCreate: "edit" | "create";
-  close?: () => void;
-}> = ({
-  postId,
-  parentCommentId,
-  setCurrentParentCommentId,
-  editOrCreate,
-  commentContent,
-  close,
-}) => {
-  const formRef = useRef<HTMLFormElement>(null);
-  const { control, handleSubmit, reset } = useForm<createCommentForm>({
-    mode: "onSubmit",
-    resolver: zodResolver(createCommentSchema),
-    defaultValues: {
-      content: commentContent,
-    },
-  });
-  const utils = trpc.useContext();
-  const createCommentMutation = trpc.post.createComment.useMutation({
-    onSuccess: (data) => {
-      setCurrentParentCommentId(data.id);
-      utils.post.getComments.invalidate();
-      reset({ content: commentContent });
-    },
-  });
-
-  const editCommentMutation = trpc.post.editComment.useMutation({
-    onSuccess: (data) => {
-      setCurrentParentCommentId(data.id);
-      if (close) close();
-      utils.post.getComments.invalidate();
-      reset({ content: commentContent });
-    },
-  });
-
-  const onSubmit = (data: createCommentForm) => {
-    if (editOrCreate === "create") {
-      createCommentMutation.mutate({
-        postId,
-        content: data.content,
-        parentCommentId,
-      });
-    } else {
-      if (parentCommentId === null) return;
-      editCommentMutation.mutate({
-        commentId: parentCommentId,
-        content: data.content,
-      });
-    }
-  };
-
-  return (
-    <div
-      className={cx(
-        "mx-auto w-full max-w-3xl rounded bg-zinc-800 md:max-w-5xl",
-        parentCommentId === null ? "my-2 p-3" : "grow-on-mount"
-      )}
-    >
-      <form ref={formRef} onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          control={control}
-          name="content"
-          render={({ field, fieldState }) => (
-            <>
-              <div
-                className={cx(
-                  "overflow-hidden text-red-400 transition-all",
-                  fieldState.isDirty && fieldState.error?.message
-                    ? "h-8"
-                    : "h-0"
-                )}
-              >
-                {fieldState.isDirty && fieldState.error?.message}
-              </div>
-              <textarea
-                autoComplete="off"
-                {...field}
-                className={cx(
-                  "max-h-[50vh] min-h-[5rem] w-full overflow-y-scroll rounded border-2 bg-transparent p-1 text-zinc-200 outline-none disabled:contrast-50",
-                  fieldState.isDirty && fieldState.error
-                    ? "border-red-600"
-                    : "border-zinc-500 focus:border-zinc-300"
-                )}
-                placeholder="Your nice, helpful and engaging comment"
-                disabled={
-                  createCommentMutation.isLoading ||
-                  editCommentMutation.isLoading
-                }
-              ></textarea>
-            </>
-          )}
-        />
-        <div className="ml-auto flex w-32 justify-end">
-          <Loading
-            show={
-              createCommentMutation.isLoading || editCommentMutation.isLoading
-            }
-            size="small"
-          />
-          <NotBannedOnlyButton
-            onClick={() => {
-              formRef.current?.requestSubmit();
-            }}
-            Child={(props) => (
-              <button
-                {...props}
-                disabled={
-                  createCommentMutation.isLoading ||
-                  editCommentMutation.isLoading
-                }
-                type="button"
-                className="my-2 w-24 rounded-md bg-indigo-800 p-2 text-white transition-colors hover:bg-indigo-900 disabled:bg-zinc-500"
-              >
-                {editOrCreate === "create" ? "Submit" : "Save"}
-              </button>
-            )}
-          />
-        </div>
-      </form>
     </div>
   );
 };
