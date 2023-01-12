754a61b9ba4ea18a482f3cd6e815731e65f56c23
HatulaPro <shalev280804@gmail.com>
2023-01-12 11:59:31 +0200

    Adding verification page

---
+++ src/pages/google2f45d386fc16ad4a.html.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const GoogleVerificationPage: NextPage = () => {
+  return <>google-site-verification: google2f45d386fc16ad4a.html</>;
+};
+
+export default GoogleVerificationPage;
