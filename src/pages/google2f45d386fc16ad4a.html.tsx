63dfccebbbf7953d7fcc31177347feb674fb82b5
HatulaPro <shalev280804@gmail.com>
2023-01-12 12:00:20 +0200

    Revert "Adding verification page"
    This reverts commit 754a61b9ba4ea18a482f3cd6e815731e65f56c23.

--- src/pages/google2f45d386fc16ad4a.html.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from "next";
-
-const GoogleVerificationPage: NextPage = () => {
-  return <>google-site-verification: google2f45d386fc16ad4a.html</>;
-};
-
-export default GoogleVerificationPage;
