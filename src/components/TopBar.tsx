1451c1c85133a0b449a19c31c9108ed5a4544ab6
HatulaPro <shalev280804@gmail.com>
2023-01-12 11:14:49 +0200

    Updated topbar style

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -35,7 +35,7 @@ export const TopBar: React.FC = () => {
   return (
     <div
       className={cx(
-        "fixed top-0 z-50 flex w-full flex-row-reverse justify-between bg-zinc-700 p-2 text-white md:flex-row",
+        "fixed top-0 z-50 flex w-full flex-row-reverse justify-between border-b-2 border-zinc-700 bg-zinc-900 bg-opacity-90 p-2 text-white md:flex-row",
         searchBarOpen ? "gap-0 md:gap-3" : "gap-3"
       )}
     >
